24c2e3341219011ef1538fbf892678f7093c02da
pietrostraneo <straneo_p@yahoo.it>
2024-05-21 15:15:54 +0200

    Code now also includes a comment for clarity.

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
+// Load environment variables from.env file
 require('dotenv').config();
+
 const http = require("http");
+
+// Set the port and host from environment variables or use default values
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+
+// Set the default greeting message from environment variable or use "Hello World!"
 const hello = process.env.HELLO || "Hello World!";
+
+// Function to get a random element from an array
 function getRandomElement(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 }
 
-const inspirational = ['Sogna in grande.', 'Credi in te.', 'Vivi con passione.', 'Sii te stesso.', 'Mai arrendersi.', 'Crea il tuo destino.'];
+// Array of inspirational quotes
+const inspirational = [
+    'Sogna in grande.',
+    'Credi in te.',
+    'Vivi con passione.',
+    'Sii te stesso.',
+    'Mai arrendersi.',
+    'Crea il tuo destino.'
+];
 
-
-
+// Create an HTTP server and listen on the specified port and host
 http.createServer((req, res) => {
+    // Set the HTTP response header with content type as text/html
     res.writeHead(200, { "Content-Type": "text/html" });
+
+    // End the response with a random inspirational quote
     res.end(`<h1>${getRandomElement(inspirational)}</h1>`);
+
+    // Start listening on the specified port and host
 }).listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
     console.log(`Server running at http://${host}:${port}/`);
-})
+});
