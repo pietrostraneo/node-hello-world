cf0a5bb17b0a310b9b446deec3a2e19a9021fbf1
pietrostraneo <straneo_p@yahoo.it>
2024-05-21 15:12:47 +0200

    Refactor response to serve random inspirational message
    Replace static "hello" message with a randomly chosen inspirational quote to provide varied and motivational content to users.

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
-const hello = process.env.HELLO || "Hello world not found!"
+const hello = process.env.HELLO || "Hello world not found!";
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+const inspirational = ['Sogna in grande.', 'Credi in te.', 'Vivi con passione.', 'Sii te stesso.', 'Mai arrendersi.', 'Crea il tuo destino.'];
+
+
 
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<h1>${hello}</h1>`);
+    res.end(`<h1>${getRandomElement(inspirational)}</h1>`);
 }).listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
     console.log(`Server running at http://${host}:${port}/`);
