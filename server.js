dcb769926dca41a935b4eeb53d271d225308f6d1
pietrostraneo <straneo_p@yahoo.it>
2024-05-21 15:03:34 +0200

    Update environment configuration and server response
    Add HELLO variable to .env file and incorporate its value into server response to allow for dynamic customization.

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+const hello = process.env.HELLO || "Hello world not found!"
 
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Hello World</h1>");
+    res.end(`<h1>${hello}</h1>`);
 }).listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`;
     console.log(`Server running at http://${host}:${port}/`);
