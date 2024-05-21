d5d70b8650dd4494fb93d4bf5ac07996032513d4
pietrostraneo <straneo_p@yahoo.it>
2024-05-21 15:00:59 +0200

    Refactor server setup for clarity and simplicity
    Consolidated server creation and listening logic.

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello World</h1>");
+}).listen(port, host, () => {
+    const serverUrl = `http://${host}:${port}`;
+    console.log(`Server running at http://${host}:${port}/`);
+})
